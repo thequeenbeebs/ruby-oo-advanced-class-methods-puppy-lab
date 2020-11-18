ff439ca29f2ecf3af5aa3b97a61e22ca24a88a1f
Blaire Baker <bakerblaire@gmail.com>
2020-11-17 22:11:57 -0600

    commit tree 5658c4bdeaa2d229df4670196cb39474b979ee27

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+end
